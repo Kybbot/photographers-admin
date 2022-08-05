2fbb827bcc43a1db142ad1fe672b16323282ded3
Артём Одийко <odiykoartem@gmail.com>
2022-08-05 18:08:20 +0300

    Added test script

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App: React.FC = () => {
+	return (
+		<div className="App">
+			<h1>App</h1>
+			<p>Lorem, ipsum.</p>
+			<img src="./sds.jpg" alt="" />
+		</div>
+	);
+};
+
+export default App;
