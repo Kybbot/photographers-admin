49f344c6552d344758dabce4e10a6822b6204cc6
Артём Одийко <odiykoartem@gmail.com>
2022-08-09 18:09:39 +0300

    Added Form for new photos

---
+++ src/components/NewPhotosForm.tsx
@@ -0,0 +1,53 @@
+import React, { ChangeEvent, FormEvent } from "react";
+
+export const NewPhotosForm = () => {
+	const [files, setFiles] = React.useState<FileList | File[] | []>();
+
+	const filesHandler = (event: ChangeEvent<HTMLInputElement>) => {
+		if (event.target.files) {
+			setFiles(event.target.files);
+		}
+	};
+
+	const removeFile = (name: string) => {
+		setFiles((prev) => {
+			if (prev) {
+				return Array.from(prev).filter((item) => item.name !== name);
+			}
+		});
+	};
+
+	const fromHandler = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		console.log(files);
+		setFiles([]);
+	};
+
+	return (
+		<form className="form" onSubmit={fromHandler}>
+			<div className="form__files">
+				<input type="file" accept=".png,.jpg,.webp" multiple id="newPhotos" name="newPhotos" onChange={filesHandler} />
+				<label htmlFor="newPhotos" className="btn">
+					Choose Photos
+				</label>
+			</div>
+			<div className="form__photos">
+				{files && files.length > 0
+					? Array.from(files).map((file, index) => (
+							<div className="form__wrapper" key={index}>
+								<img className="form__photo" src={URL.createObjectURL(file)} alt={file.name} />
+								<button className="form__remove btn" type="button" onClick={() => removeFile(file.name)}>
+									Remove
+								</button>
+							</div>
+					  ))
+					: "No Photos"}
+			</div>
+			<button type="submit" className="btn">
+				Upload Photos
+			</button>
+		</form>
+	);
+};
+
+export default NewPhotosForm;
