1d4532dd7ca278ccdd1eafe299dddeed2e3d8050
Артём Одийко <odiykoartem@gmail.com>
2022-08-17 18:49:02 +0300

    Added scroll up when changing page

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
+import { ScrollToTop } from "./components/ScrollToTop";
 import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<AuthProvider>
+				<ScrollToTop />
 				<App />
 			</AuthProvider>
 		</BrowserRouter>
