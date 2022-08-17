5cad4ffefed9e9f06e6c2a296be01eb1b289cf82
Артём Одийко <odiykoartem@gmail.com>
2022-08-17 18:13:31 +0300

    Added handlers for inputs and form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent } from "react";
+
+import { useAuthContext } from "../context/AuthContext";
 
 const Login: React.FC = () => {
+	const { saveToken } = useAuthContext();
+
+	const [formState, setFormState] = React.useState({
+		login: "",
+		password: "",
+	});
+
+	const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target;
+
+		setFormState((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
+
+	const handleFormSubmit = async (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+
+		// saveToken({ accessToken: "token" });
+
+		const response = await fetch("https://splastun2.node.shpp.me/api/login", {
+			method: "POST",
+			headers: {
+				"content-type": "application/json;charset=UTF-8",
+			},
+			body: JSON.stringify(formState),
+		});
+
+		console.log(response);
+
+		type dataType = {
+			token: string;
+			user: {
+				person_id: number;
+				login: string;
+				fullname: string;
+				email: string;
+			};
+		};
+
+		const data = (await response.json()) as dataType;
+
+		console.log(data);
+	};
+
 	return (
 		<section className="login" aria-labelledby="loginSectionTitle">
 			<div className="login__container">
@@ -8,14 +56,30 @@ const Login: React.FC = () => {
 					Log in
 				</h1>
 				<p className="login__text">Enter your credentials to acces yout account.</p>
-				<form className="form login__form">
+				<form className="form login__form" onSubmit={handleFormSubmit}>
 					<label className="form__label" htmlFor="login">
 						Login
-						<input className="form__input" type="text" id="login" name="login" autoComplete="username" />
+						<input
+							onChange={handleInputChange}
+							value={formState.login}
+							className="form__input"
+							type="text"
+							id="login"
+							name="login"
+							autoComplete="username"
+						/>
 					</label>
 					<label className="form__label" htmlFor="password">
 						Password
-						<input className="form__input" type="password" id="password" name="password" autoComplete="new-password" />
+						<input
+							onChange={handleInputChange}
+							value={formState.password}
+							className="form__input"
+							type="password"
+							id="password"
+							name="password"
+							autoComplete="new-password"
+						/>
 					</label>
 					<button type="submit" className="btn">
 						Login
