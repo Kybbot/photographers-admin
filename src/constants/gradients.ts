4b451ff6bb6f26dfde954b581506b25fe61798c8
Артём Одийко <odiykoartem@gmail.com>
2022-08-24 16:00:21 +0300

    Added array of gradients

---
+++ src/constants/gradients.ts
@@ -0,0 +1,11 @@
+export const gradients = [
+	" radial-gradient( circle farthest-corner at 12.3% 19.3%,  rgba(85,88,218,1) 0%, rgba(95,209,249,1) 100.2% )",
+	" linear-gradient(to right, rgb(242, 112, 156), rgb(255, 148, 114))",
+	" linear-gradient(to right, rgb(182, 244, 146), rgb(51, 139, 147))",
+	" linear-gradient( 109.6deg,  rgba(253,199,141,1) 11.3%, rgba(249,143,253,1) 100.2% )",
+	" linear-gradient( 109.6deg,  rgba(156,252,248,1) 11.2%, rgba(110,123,251,1) 91.1% )",
+	" linear-gradient(to top, #a18cd1 0%, #fbc2eb 100%)",
+	" linear-gradient( 109.6deg,  rgba(218,185,252,1) 11.2%, rgba(125,89,252,1) 91.1% )",
+	" radial-gradient( circle farthest-corner at 10% 20%,  rgba(253,193,104,1) 0%, rgba(251,128,128,1) 90% )",
+	" linear-gradient( 109.6deg,  rgba(48,207,208,1) 11.2%, rgba(51,8,103,1) 92.5% )",
+];
