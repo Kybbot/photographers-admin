f7b42c932859631113c8355486efeec2b3ec46b9
Артём Одийко <odiykoartem@gmail.com>
2022-08-25 18:22:21 +0300

    Updated useModal hook

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,28 +1,23 @@
 import React from "react";
 
-type useModalProps = (
-	modalRef: React.RefObject<HTMLDivElement>,
-	openBtnRef: React.RefObject<HTMLButtonElement>
-) => {
+type useModalProps = (openBtnRef?: React.RefObject<HTMLButtonElement>) => {
 	isActive: boolean;
 	openModal: () => void;
 	closeModal: () => void;
 };
 
-export const useModal: useModalProps = (modalRef, openBtnRef) => {
+export const useModal: useModalProps = (openBtnRef) => {
 	const [isActive, setActive] = React.useState(false);
 
 	const openModal = React.useCallback(() => {
 		document.body.style.overflow = "hidden";
-		const btn = modalRef.current?.querySelector(".modal__btn") as HTMLElement;
-		btn.focus();
-		setActive((prev) => !prev);
-	}, [modalRef]);
+		setActive(true);
+	}, []);
 
 	const closeModal = React.useCallback(() => {
 		document.body.style.overflow = "initial";
-		openBtnRef.current?.focus();
-		setActive((prev) => !prev);
+		openBtnRef?.current?.focus();
+		setActive(false);
 	}, [openBtnRef]);
 
 	return { isActive, openModal, closeModal };
