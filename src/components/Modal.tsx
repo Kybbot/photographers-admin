e0c88601b80e73523175defe3edae9c5dbbd497b
Артём Одийко <odiykoartem@gmail.com>
2022-08-29 18:50:47 +0300

    Added description

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 type ModalProps = {
 	title: string;
+	description?: string;
 	active: boolean;
 	closeModal: () => void;
 	children: React.ReactNode;
 };
 
-export const Modal: React.FC<ModalProps> = ({ title, active, closeModal, children }) => {
+export const Modal: React.FC<ModalProps> = ({ title, description, active, closeModal, children }) => {
 	const closeBtnRef = React.useRef<HTMLButtonElement>(null);
 
 	React.useEffect(() => {
@@ -20,10 +21,13 @@ export const Modal: React.FC<ModalProps> = ({ title, active, closeModal, childre
 		<div aria-hidden={!active} className={`modal ${active ? "modal--visible" : ""}`}>
 			<div className="modal__content" role="dialog" aria-modal="true" aria-label="Modal window">
 				<div className="modal__header">
-					<h2 className="modal__titel">{title}</h2>
-					<button ref={closeBtnRef} className="btn modal__btn" onClick={closeModal}>
-						Close
-					</button>
+					<div className="modal__wrapper">
+						<h2 className="modal__titel">{title}</h2>
+						<button ref={closeBtnRef} className="btn modal__btn" onClick={closeModal}>
+							Close
+						</button>
+					</div>
+					<p className="modal__description">{description}</p>
 				</div>
 				<div className="modal__body">{children}</div>
 			</div>
