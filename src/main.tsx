27b0a850c2db505ee6469598d14a8aac9670755b
Артём Одийко <odiykoartem@gmail.com>
2022-08-31 10:25:06 +0300

    Removed redux

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
 
 import { AuthProvider } from "./context/AuthContext";
 
 import App from "./App";
 import { ScrollToTop } from "./components";
 
-import store from "./redux/store";
-
 import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<>
-		<Provider store={store}>
-			<BrowserRouter>
-				<AuthProvider>
-					<ScrollToTop />
-					<App />
-				</AuthProvider>
-			</BrowserRouter>
-		</Provider>
+		<BrowserRouter>
+			<AuthProvider>
+				<ScrollToTop />
+				<App />
+			</AuthProvider>
+		</BrowserRouter>
 	</>
 );
